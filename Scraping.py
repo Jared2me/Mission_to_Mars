2aeb56dd5f825c63ec64f31c10ba1f68dcdfb651
Jared Toomey <jaredtoomey@icloud.com>
2022-08-25 17:31:53 -0400

    Update

--- Scraping.py
+++ Scraping.py
@@ -22,7 +22,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
     }
 
     # Stop webdriver and return data
@@ -99,6 +100,32 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemispheres(browser):
+
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+    
+    links = browser.find_by_css('a.product-item img')
+
+    for i in range(len(links)):
+        #dict
+        hemisphere = {}
+        #click
+        browser.find_by_css('a.product-item img')[i].click()
+        #sfind / extract imgs
+        samp_el = browser.links.find_by_text('Sample').first
+        hemisphere['img_url'] = samp_el['href']
+        #titles
+        hemisphere['title'] = browser.find_by_css('h2.title').text
+        #append
+        hemisphere_image_urls.append(hemisphere)
+        #back
+        browser.back()
+        
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
